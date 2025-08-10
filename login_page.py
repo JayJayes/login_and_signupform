904db6030061e73bee555b58251e6377e9c98749
Jayesh badgujar <152469249+JayJayes@users.noreply.github.com>
2025-08-10 04:53:21 +0000

    login page background change

--- login_page.py
+++ login_page.py
@@ -4,126 +4,122 @@ import pymysql
 import os
 from signup_page import SignUp
 import credentials as cr
+from PIL import Image, ImageTk
 
 class login_page:
     def __init__(self, root):
         self.window = root
         self.window.title("Log In")
-        # Set the window size
-        # Here 0,0 represents the starting point of the window 
         self.window.geometry("1280x800+0+0")
-        self.window.config(bg = "white")
+        self.window.config(bg="white")
 
-        #============================================================================
-        #==============================DESIGN PART===================================
-        #============================================================================
+        # ====================== Background Image =======================
+        self.bg_img = ImageTk.PhotoImage(file="Images/photo1.jpeg")
+        background = Label(self.window, image=self.bg_img).place(x=0, y=0, relwidth=1, relheight=1)
 
-        self.frame1 = Frame(self.window, bg="yellow")
-        self.frame1.place(x=0, y=0, width=450, relheight = 1)
+        # ====================== Center Frame ===========================
+        frame = Frame(self.window, bg="white")
+        frame.place(x=350, y=150, width=500, height=500)
 
-        label1 = Label(self.frame1, text= "Py", font=("times new roman", 40, "bold"), bg="yellow", fg="red").place(x=100,y=300)
-        label2 = Label(self.frame1, text= "Seek", font=("times new roman", 40, "bold"), bg="yellow", fg="RoyalBlue1").place(x=162,y=300)
-        label3 = Label(self.frame1, text= "It's all about Python", font=("times new roman", 13, "bold"), bg="yellow", fg="brown4").place(x=100,y=360)
+        # Title
+        title1 = Label(frame, text="Log In", font=("times new roman", 25, "bold"), bg="white").place(x=20, y=10)
+        title2 = Label(frame, text="Welcome Back!", font=("times new roman", 13), bg="white", fg="gray").place(x=20, y=50)
 
-        #=============Entry Field & Buttons============
+        # Email
+        email_label = Label(frame, text="Email Address", font=("helvetica", 15, "bold"), bg="white").place(x=20, y=100)
+        self.email_entry = Entry(frame, font=("arial"))
+        self.email_entry.place(x=20, y=130, width=420)
 
-        self.frame2 = Frame(self.window, bg = "gray95")
-        self.frame2.place(x=450,y=0,relwidth=1, relheight=1)
+        # Password
+        password_label = Label(frame, text="Password", font=("helvetica", 15, "bold"), bg="white").place(x=20, y=180)
+        self.password_entry = Entry(frame, font=("arial"), show="*")
+        self.password_entry.place(x=20, y=210, width=420)
 
-        self.frame3 = Frame(self.frame2, bg="white")
-        self.frame3.place(x=140,y=150,width=500,height=450)
+        # Log In Button
+        self.login_button = Button(frame, text="Log In", command=self.login_func, font=("times new roman", 18, "bold"),
+                                   bd=0, cursor="hand2", bg="blue", fg="white").place(x=120, y=260, width=250)
 
-        self.email_label = Label(self.frame3,text="Email Address", font=("helvetica",20,"bold"),bg="white", fg="gray").place(x=50,y=40)
-        self.email_entry = Entry(self.frame3,font=("times new roman",15,"bold"),bg="white",fg="gray")
-        self.email_entry.place(x=50, y=80, width=300)
+        # Forgot Password Button
+        self.forgotten_pass = Button(frame, text="Forgotten password?", command=self.forgot_func,
+                                     font=("times new roman", 10, "bold"), bd=0, cursor="hand2",
+                                     bg="white", fg="blue").place(x=160, y=310, width=150)
 
-        self.password_label = Label(self.frame3,text="Password", font=("helvetica",20,"bold"),bg="white", fg="gray").place(x=50,y=120)
-        self.password_entry = Entry(self.frame3,font=("times new roman",15,"bold"),bg="white",fg="gray",show="*")
-        self.password_entry.place(x=50, y=160, width=300)
+        # Create Account Button
+        self.create_button = Button(frame, text="Create New Account", command=self.redirect_window,
+                                    font=("times new roman", 18, "bold"), bd=0, cursor="hand2",
+                                    bg="green2", fg="white").place(x=120, y=360, width=250)
 
-        #================Buttons===================
-        self.login_button = Button(self.frame3,text="Log In",command=self.login_func,font=("times new roman",15, "bold"),bd=0,cursor="hand2",bg="blue",fg="white").place(x=50,y=200,width=300)
-
-        self.forgotten_pass = Button(self.frame3,text="Forgotten password?",command=self.forgot_func,font=("times new roman",10, "bold"),bd=0,cursor="hand2",bg="white",fg="blue").place(x=125,y=260,width=150)
-
-        self.create_button = Button(self.frame3,text="Create New Account",command=self.redirect_window,font=("times new roman",18, "bold"),bd=0,cursor="hand2",bg="green2",fg="white").place(x=80,y=320,width=250)
-
-
+    # ====================== Functionality ==============================
     def login_func(self):
-        if self.email_entry.get()=="" or self.password_entry.get()=="":
-            messagebox.showerror("Error!","All fields are required",parent=self.window)
+        if self.email_entry.get() == "" or self.password_entry.get() == "":
+            messagebox.showerror("Error!", "All fields are required", parent=self.window)
         else:
             try:
-                connection=pymysql.connect(host=cr.host, user=cr.user, password=cr.password, database=cr.database)
+                connection = pymysql.connect(host=cr.host, user=cr.user, password=cr.password, database=cr.database)
                 cur = connection.cursor()
-                cur.execute("select * from student_register where email=%s and password=%s",(self.email_entry.get(),self.password_entry.get()))
-                row=cur.fetchone()
-                if row == None:
-                    messagebox.showerror("Error!","Invalid USERNAME & PASSWORD",parent=self.window)
+                cur.execute("select * from student_register where email=%s and password=%s",
+                            (self.email_entry.get(), self.password_entry.get()))
+                row = cur.fetchone()
+                if row is None:
+                    messagebox.showerror("Error!", "Invalid USERNAME & PASSWORD", parent=self.window)
                 else:
-                    messagebox.showinfo("Success","Wellcome ",parent=self.window)
-                    # Clear all the entries
+                    messagebox.showinfo("Success", "Welcome", parent=self.window)
                     self.reset_fields()
-                    
                     connection.close()
-
             except Exception as e:
-                messagebox.showerror("Error!",f"Error due to {str(e)}",parent=self.window)
+                messagebox.showerror("Error!", f"Error due to {str(e)}", parent=self.window)
 
     def forgot_func(self):
-        if self.email_entry.get()=="":
-            messagebox.showerror("Error!", "Please enter your Email Id",parent=self.window)
+        if self.email_entry.get() == "":
+            messagebox.showerror("Error!", "Please enter your Email Id", parent=self.window)
         else:
             try:
                 connection = pymysql.connect(host=cr.host, user=cr.user, password=cr.password, database=cr.database)
                 cur = connection.cursor()
                 cur.execute("select * from student_register where email=%s", self.email_entry.get())
-                row=cur.fetchone()
-                if row == None:
+                row = cur.fetchone()
+                if row is None:
                     messagebox.showerror("Error!", "Email Id doesn't exists")
                 else:
                     connection.close()
-                    
-                    #=========================SECOND WINDOW===============================
-                    #------------Toplevel:create a window top of another window-----------
-                    #------------focus_force:Helps to to focus on the current window------
-                    #-----Grab:Helps to grab the current window until user ungrab it------
 
-                    self.root=Toplevel()
+                    self.root = Toplevel()
                     self.root.title("Forget Password?")
                     self.root.geometry("400x440+450+200")
                     self.root.config(bg="white")
                     self.root.focus_force()
                     self.root.grab_set()
 
-                    title3 = Label(self.root,text="Change your password",font=("times new roman",20,"bold"),bg="white").place(x=10,y=10)
+                    Label(self.root, text="Change your password", font=("times new roman", 20, "bold"),
+                          bg="white").place(x=10, y=10)
+                    Label(self.root, text="It's quick and easy", font=("times new roman", 12),
+                          bg="white").place(x=10, y=45)
 
-                    title4 = Label(self.root,text="It's quick and easy",font=("times new roman",12),bg="white").place(x=10,y=45)
+                    Label(self.root, text="Select your question", font=("times new roman", 15, "bold"),
+                          bg="white").place(x=10, y=85)
 
-                    title5 = Label(self.root, text="Select your question", font=("times new roman", 15, "bold"), bg="white").place(x=10,y=85)
-
-                    self.sec_ques = ttk.Combobox(self.root,font=("times new roman",13),state='readonly',justify=CENTER)
-                    self.sec_ques['values'] = ("Select","What's your pet name?","Your first teacher name","Your birthplace", "Your favorite movie")
-                    self.sec_ques.place(x=10,y=120, width=270)
+                    self.sec_ques = ttk.Combobox(self.root, font=("times new roman", 13), state='readonly', justify=CENTER)
+                    self.sec_ques['values'] = ("Select", "What's your pet name?", "Your first teacher name",
+                                               "Your birthplace", "Your favorite movie")
+                    self.sec_ques.place(x=10, y=120, width=270)
                     self.sec_ques.current(0)
-                    
-                    title6 = Label(self.root, text="Answer", font=("times new roman", 15, "bold"), bg="white").place(x=10,y=160)
 
-                    self.ans = Entry(self.root,font=("arial"))
-                    self.ans.place(x=10,y=195,width=270)
+                    Label(self.root, text="Answer", font=("times new roman", 15, "bold"),
+                          bg="white").place(x=10, y=160)
+                    self.ans = Entry(self.root, font=("arial"))
+                    self.ans.place(x=10, y=195, width=270)
 
-                    title7 = Label(self.root, text="New Password", font=("times new roman", 15, "bold"), bg="white").place(x=10,y=235)
+                    Label(self.root, text="New Password", font=("times new roman", 15, "bold"),
+                          bg="white").place(x=10, y=235)
+                    self.new_pass = Entry(self.root, font=("arial"))
+                    self.new_pass.place(x=10, y=270, width=270)
 
-                    self.new_pass = Entry(self.root,font=("arial"))
-                    self.new_pass.place(x=10,y=270,width=270)
-
-                    self.create_button = Button(self.root,text="Submit",command=self.change_pass,font=("times new roman",18, "bold"),bd=0,cursor="hand2",bg="green2",fg="white").place(x=95,y=340,width=200)
-                    #=========================================================================
+                    Button(self.root, text="Submit", command=self.change_pass, font=("times new roman", 18, "bold"),
+                           bd=0, cursor="hand2", bg="green2", fg="white").place(x=95, y=340, width=200)
 
             except Exception as e:
                 messagebox.showerror("Error", f"{e}")
-                
-      
+
     def change_pass(self):
         if self.email_entry.get() == "" or self.sec_ques.get() == "Select" or self.new_pass.get() == "":
             messagebox.showerror("Error!", "Please fill the all entry field correctly")
@@ -131,40 +127,35 @@ class login_page:
             try:
                 connection = pymysql.connect(host=cr.host, user=cr.user, password=cr.password, database=cr.database)
                 cur = connection.cursor()
-                cur.execute("select * from student_register where email=%s and question=%s and answer=%s", (self.email_entry.get(),self.sec_ques.get(),self.ans.get()))
-                row=cur.fetchone()
+                cur.execute("select * from student_register where email=%s and question=%s and answer=%s",
+                            (self.email_entry.get(), self.sec_ques.get(), self.ans.get()))
+                row = cur.fetchone()
 
-                if row == None:
+                if row is None:
                     messagebox.showerror("Error!", "Please fill the all entry field correctly")
                 else:
-                    try:
-                        cur.execute("update student_register set password=%s where email=%s", (self.new_pass.get(),self.email_entry.get()))
-                        connection.commit()
+                    cur.execute("update student_register set password=%s where email=%s",
+                                (self.new_pass.get(), self.email_entry.get()))
+                    connection.commit()
+                    messagebox.showinfo("Successful", "Password has changed successfully")
+                    connection.close()
+                    self.reset_fields()
+                    self.root.destroy()
 
-                        messagebox.showinfo("Successful", "Password has changed successfully")
-                        connection.close()
-                        self.reset_fields()
-                        self.root.destroy()
-
-                    except Exception as er:
-                        messagebox.showerror("Error!", f"{er}")
-                        
             except Exception as er:
-                        messagebox.showerror("Error!", f"{er}")
-            
+                messagebox.showerror("Error!", f"{er}")
 
     def redirect_window(self):
         self.window.destroy()
-        # Importing the signup window.
-        # The page must be in the same directory
         root = Tk()
         obj = SignUp(root)
         root.mainloop()
 
     def reset_fields(self):
-        self.email_entry.delete(0,END)
-        self.password_entry.delete(0,END)
-# The main function
+        self.email_entry.delete(0, END)
+        self.password_entry.delete(0, END)
+
+
 if __name__ == "__main__":
     root = Tk()
     obj = login_page(root)
